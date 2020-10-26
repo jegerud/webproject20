a293f0351be98ab8d86df5c448071d8c9aa3760b
casperfg <casperfg@stud.ntnu.no>
2020-10-26 14:57:13 +0100

    creddit logo sends you home'

--- client/src/menuBar.js
+++ client/src/menuBar.js
@@ -6,7 +6,7 @@ export class menuBar extends LitElement {
        return html`
     <link rel="stylesheet" href="./src/styles/header.css">
     <div class="header">
-     <a href="" class="logo">Creddit</a>
+     <a href="/" class="logo">Creddit</a>
    <div class="header-right">
     <a href="">Sign up</a>
     <a class="active" href="">Log in</a>
