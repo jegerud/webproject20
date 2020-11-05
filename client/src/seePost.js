ce1b01aa49ba81689253a6168f103d35734e018f
Kristian Jegerud <kristjeg@stud.ntnu.no>
2020-11-05 12:28:45 +0100

    I dont know

--- client/src/seePost.js
+++ client/src/seePost.js
@@ -33,28 +33,28 @@ export class seePost extends LitElement {
         return html`
         ${this.data.map(item => html`
         <div class="post-content">
-                                <h1>${item.title}</h1> <!-- Denne må styles -->
-								  <img src="https://via.placeholder.com/400x150/FFB6C1/000000" alt="post-image" class="img-responsive post-image">
-								  <div class="post-container">
-									<img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="user" class="profile-photo-md pull-left">
-									<div class="post-detail">
-									  <div class="user-info">
-										<h5><a href="timeline.html" class="profile-link">Alexis Clark</a> <span class="following">following</span></h5>
-										<p class="text-muted">Published a photo about 7 mins ago</p>
-									  </div>
-									  <div class="reaction">
-										<a class="btn text-green"><i class="fa fa-thumbs-up"></i> 13</a>
-										<a class="btn text-red"><i class="fa fa-thumbs-down"></i> 0</a>
-									  </div>
-									  <div class="line-divider"></div>
-									  <div class="post-text">
-										<p> ${item.content} <i class="em em-anguished"></i> <i class="em em-anguished"></i> <i class="em em-anguished"></i></p>
-									  </div>
-									  <div class="line-divider"></div>
-									  <comments-all></comments-all>
-									</div>
-								  </div>
-                                </div>
+          <h1>${item.title}</h1>
+					<img src="https://via.placeholder.com/400x150/FFB6C1/000000" alt="post-image" class="img-responsive post-image">
+					<div class="post-container">
+					<img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="user" class="profile-photo-md pull-left">
+					<div class="post-detail">
+					  <div class="user-info">
+						<h5><a href="timeline.html" class="profile-link">Alexis Clark</a> <span class="following">following</span></h5>
+						<p class="text-muted">Published a photo about 7 mins ago</p>
+					  </div>
+					  <div class="reaction">
+						<a class="btn text-green"><i class="fa fa-thumbs-up"></i> 13</a>
+						<a class="btn text-red"><i class="fa fa-thumbs-down"></i> 0</a>
+					  </div>
+					  <div class="line-divider"></div>
+					  <div class="post-text">
+						<p> ${item.content} <i class="em em-anguished"></i> <i class="em em-anguished"></i> <i class="em em-anguished"></i></p>
+					  </div>
+					  <div class="line-divider"></div>
+					  <comments-all></comments-all>
+					</div>
+					</div>
+        </div>
 
         `)}
         `
