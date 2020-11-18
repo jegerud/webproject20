6647b80e7d113d0201461cf6e5341713d58048e7
Kristian Jegerud <kristjeg@stud.ntnu.no>
2020-11-18 14:41:40 +0100

    On the way to be able to delete users

--- client/src/subAllusers.js
+++ client/src/subAllusers.js
@@ -3,7 +3,9 @@ import { LitElement, html, css } from '../node_modules/lit-element/lit-element';
 export class subAllusers extends LitElement {
     static get properties() {
         return {
-            data: {type: Array}
+            data: {type: Array},
+            userid: {type: Number},
+            user: {type: Object},
         }
     }
 
@@ -11,6 +13,32 @@ export class subAllusers extends LitElement {
         super();
         this.data = [];
         this.getResource();
+        this.getUserid();
+        this.getUsername();
+    }
+
+    getUserid() {
+        this.userid = localStorage.getItem('userid');
+        if (this.userid !== undefined && this.userid !== null) {
+           this.loggedIn = true;
+        } else {
+           this.loggedIn = false;
+        }
+    }
+
+    getUsername() {
+        fetch(`http://localhost:8081/getUsername/${this.userid}`, {
+            method: 'GET'
+        })
+        .then((response) => response.text())
+        .then((responseText) => {
+            this.user = JSON.parse(responseText);
+        })
+        .catch((error) => {
+            console.log("The data could not be fetched");
+            console.error(error);
+        });
+
     }
 
     async getResource() {
@@ -27,13 +55,93 @@ export class subAllusers extends LitElement {
         });
     }
 
+    deleteUser(text) {
+        var current = this;
+        console.log(text, " will be deleted");
+        fetch(`http://localhost:8081/getUserid/${text}`, {
+            method: 'GET',
+        }).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        }).then(function (data) {
+            current.deleteComments(data);
+    
+        }).catch(function (error) {
+            console.warn('Something went wrong.', error);
+        });
+    }
+
+    deleteComments(uid) {
+        let rawData = {
+            "userid": uid
+        }
+        fetch('http://localhost:8081/deletecomments', {
+            method: 'POST',
+            body: JSON.stringify(rawData),
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        }).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        }).then(function (data) {
+            console.log(text, "'s comments deleted");
+        }).catch(function (error) {
+            console.warn('Something went wrong.', error);
+        });
+
+        fetch('http://localhost:8081/deleteposts', {
+            method: 'POST',
+            body: JSON.stringify(rawData),
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        }).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        }).then(function (data) {
+            console.log(text, "'s comments deleted");
+        }).catch(function (error) {
+            console.warn('Something went wrong.', error);
+        });
+
+        fetch('http://localhost:8081/deleteuser', {
+            method: 'POST',
+            body: JSON.stringify(rawData),
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        }).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        }).then(function (data) {
+            console.log(text, "'s comments deleted");
+        }).catch(function (error) {
+            console.warn('Something went wrong.', error);
+        });
+    }
+
     render() {
       return html`
       <br>
       ${this.data.map(item => html`
         <a>Username:  ${item.username}</a><br>
         <a>User Type: ${item.userType}</a><br>
-      <br>`)}
+        ${item.username != this.user[0].username ? 
+            html`
+            <input id="submit" @click="${() => this.deleteUser(item.username)}" type="submit" 
+            class="btn" type="button" name="" value="Delete user"></input>
+        ` : 
+        html`<br>`}
+      <br><br>`)}
     `
     ;}
 }
